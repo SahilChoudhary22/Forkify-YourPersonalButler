019a51ee6d730dd1fb9efc66b235534f54b4a1c4
Sahil Choudhary <sahil.codes.it@gmail.com>
2020-04-10 00:44:34 +0530

    Search model and searchviews update

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,43 @@
-// Global app controller
-import num from './test'
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`)
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
+
+/** Global state of the app
+ * - search object
+ * - current recipe object
+ * - shopping list object
+ * - liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from the view
+    const query = searchView.getInput() //TODO
+    console.log(query)
+
+    if (query){
+        // 2) new search obj and add it to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        searchView.renderResults(state.search.result);
+    }
+
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+
+
+
+
---
+++ src/js/views/base.js
@@ -0,0 +1,7 @@
+//** This file is for managing DOMs */
+
+export const elements = {
+    searchInput : document.querySelector('.search__field'),
+    searchForm : document.querySelector('.search'),
+    searchResList : document.querySelector('.results__list')
+}
