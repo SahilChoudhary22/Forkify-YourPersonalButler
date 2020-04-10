526edb163c02abd053ee4032c53d33693ed29db7
Sahil Choudhary <sahil.codes.it@gmail.com>
2020-04-10 18:23:53 +0530

    large update - working on recipes

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
-import {elements} from './views/base';
+import {elements, renderLoader, clearLoader} from './views/base';
 
 /** Global state of the app
  * - search object
@@ -9,7 +10,7 @@ import {elements} from './views/base';
  * - liked recipes
  */
 const state = {};
-
+/** SEARCH CONTROLLER */
 const controlSearch = async () => {
     // 1) Get query from the view
     const query = searchView.getInput() //TODO
@@ -22,12 +23,19 @@ const controlSearch = async () => {
         // 3) Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
-
-        // 4) Search for recipes
-        await state.search.getResults();
+        renderLoader(elements.searchRees);
+        try {
+            await state.search.getResults();
 
         // 5) Render results on UI
-        searchView.renderResults(state.search.result);
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch(error) {
+            alert('Something wrong with the search...');
+            clearLoader();
+        }
+        // 4) Search for recipes
+        
     }
 
 }
@@ -37,7 +45,48 @@ elements.searchForm.addEventListener('submit', e => {
     controlSearch();
 });
 
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if(btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        // clear prev results
+        searchView.clearResults();
+        // render results of next page
+        searchView.renderResults(state.search.result, goToPage);
+        
+    }
+
+});
 
 
+/** RECIPE CONTROLLER */
+const controlRecipe = async () => {
+    // get ID from URL
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
 
+    if (id) {
+        // Prepare UI for changes
 
+        // Create new recipe object
+        state.recipe = new Recipe(id);
+
+        try {
+            await state.recipe.getRecipe();
+
+        // Calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+        // Render recipe
+            console.log(state.recipe);
+        } catch (err) {
+            alert("Error processing recipe!");
+        }
+        // Get recipe data
+        
+    }
+     
+}
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
