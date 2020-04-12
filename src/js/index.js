0102b25a9c71590fbf572f1331094c3999b8217c
Sahil Choudhary <sahil.codes.it@gmail.com>
2020-04-13 00:16:22 +0530

    likes now supports localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -167,12 +167,6 @@ elements.shopping.addEventListener('click', e => {
 
 
 
-/** LIKE CONTROLLER */
-
-//testing
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
-
 // main
 const controlLike = () => {
     if (!state.likes) state.likes = new Likes();
@@ -205,6 +199,20 @@ const controlLike = () => {
     }
     likesView.toggleLikeMenu(state.likes.getNumLikes());
 }
+
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    // Restore likes
+    state.likes.readStorage();
+
+    // Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    // Render the existing likes
+    state.likes.likes.forEach(like =>likesView.renderLike(like));
+});
 
 
 // Handling recipe button clicks
